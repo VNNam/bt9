bc0ae70918245b9e2eb9ce44941b8d9430daebd2
Nam Nguyen <goo.itpro@gmail.com>
2022-01-12 20:43:08 +0700

    jwt add in index.js

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const jwt = require('jsonwebtoken');
 router.get('/', function (req, res, next) {
   try {
     const { token } = req.signedCookies;
-    const decoded = jwt.verify(token, 'jwt-secret');
-
-    res.render('index', { title: `Hello ${decoded.name}` });
+    if (token) {
+      const decoded = jwt.verify(token, 'jwt-secret');
+      return res.render('index', { title: `Hello ${decoded.name}` });
+    }
   } catch (error) {
-    res.render('index');
+    res.clearCookie('token');
+    return res.redirect('/users/login');
   }
 });
 
